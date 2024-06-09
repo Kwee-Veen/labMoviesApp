40c2d13762d09652b6673333e6cf20e042220aee
Caoimhín <caoimhinarnott@hotmail.com>
2024-06-09 16:38:44 +0100

    Movie Details Page - included production countries

--- src/components/movieDetails/index.tsx
+++ src/components/movieDetails/index.tsx
@@ -45,6 +45,16 @@ const MovieDetails: React.FC<MovieDetailsProps> = (movie) => {
                 ))}
             </Paper>
             <Paper component="ul" sx={styles.chipSet}>
+                <li>
+                    <Chip label="Production Countries" sx={styles.chipLabel} color="secondary" />
+                </li>
+                {movie.production_countries.map((c) => (
+                    <li key={c.name}>
+                        <Chip label={c.name} />
+                    </li>
+                ))}
+            </Paper>
+            <Paper component="ul" sx={styles.chipSet}>
                 <Chip icon={<AccessTimeIcon />} label={`${movie.runtime} min.`} />
                 <Chip
                     icon={<MonetizationIcon />}
@@ -56,6 +66,7 @@ const MovieDetails: React.FC<MovieDetailsProps> = (movie) => {
                 />
                 <Chip label={`Released: ${movie.release_date}`} />
             </Paper>
+
         </>
     );
 };
